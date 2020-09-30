f0f10e8488e403e01c6f2934b26fc16268ff1a58
federicov <vaglicafederico95@gmail.com>
2020-09-30 12:17:35 +0200

    Update (upgrade with db)

---
+++ php-crude/partials/update/server.php
@@ -0,0 +1,23 @@
+<?php
+include __DIR__ .'/../database.php';
+
+if(empty($_GET['id'])){
+    die('nessun id');
+}
+
+$id = $_GET['id'];
+
+$sql = "SELECT id,room_number, floor , beds FROM stanze WHERE id = $id";
+$result = $conn->query($sql);
+
+if ($result && $result->num_rows > 0) {
+    $results=[];
+    $row = $result->fetch_assoc();
+    $results[]=$row;
+    } elseif ($result) {
+        echo "0 results";
+    } else {
+        echo "query error";
+    }
+$conn->close();
+?>
