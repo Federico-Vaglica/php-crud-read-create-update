ac4012248e39639b72e29d0d056d72849d1e13cf
federicov <vaglicafederico95@gmail.com>
2020-10-01 11:47:13 +0200

    Creating FUNCTION (php)

---
+++ php-crude/partials/functions.php
@@ -0,0 +1,22 @@
+<?php
+
+function getAll($conn , $table){
+    $sql = "SELECT * FROM $table";
+    $result = $conn->query($sql);
+
+    if ($result && $result->num_rows > 0) {
+        $results=[];
+    while($row = $result->fetch_assoc()) {
+       $results[]=$row;
+    }
+       
+    } elseif ($result) {
+        $results = [];
+    } else {
+        $result = false;
+    }
+
+    $conn->close();
+    return $results;
+}
+?>
